121c64e09461ca94c6c0dccb09997c3b644ab73f
JMJo321 <jinmahn.jo@gmail.com>
2021-01-26 21:12:08 -0800

    A-04-01B: Add columns, including information about rate code changes (#1)

--- 05_Code/02_Analysis/04_Greenergy-Program/A-04-01B_Greenergy-Program_Make-a-DT-showing-PV-Adaption-and-Greenergy-Participation.R
+++ 05_Code/02_Analysis/04_Greenergy-Program/A-04-01B_Greenergy-Program_Make-a-DT-showing-PV-Adaption-and-Greenergy-Participation.R
@@ -41,13 +41,18 @@ source(PATH_HEADER)
 # # 1. Path(s) for Data file(s)
 # # 1.1. SMUD Billing Data for RD Design
 DIR_TO.LOAD_GN <- "02_Greenergy-Program"
-FILE_TO.LOAD_GN <- "DT_Greenergy-Program.RData"
-PATH_TO.LOAD_GN <-
-  paste(PATH_DATA_ANALYSIS, DIR_TO.LOAD_GN, FILE_TO.LOAD_GN, sep = "/")
+FILE_TO.LOAD_GN_BILLING <- "DT_Greenergy-Program_Billing-Data.RData"
+PATH_TO.LOAD_GN_BILLING <-
+  paste(PATH_DATA_ANALYSIS, DIR_TO.LOAD_GN, FILE_TO.LOAD_GN_BILLING, sep = "/")
+# # 1.2. Data for Changes in Greenergy Program Participation and Residential
+# #      Rate
+FILE_TO.LOAD_GN_CHANGES <- "DT_Greenergy-Program_Changes.RData"
+PATH_TO.LOAD_GN_CHANGES <-
+  paste(PATH_DATA_ANALYSIS, DIR_TO.LOAD_GN, FILE_TO.LOAD_GN_CHANGES, sep = "/")
 
 # # 2. Path at which the DT created will be saved
 DIR_TO.SAVE <- DIR_TO.LOAD_GN
-FILE_TO.SAVE <- "DT_Greenergy-Program_With-PV-Adoption.RData"
+FILE_TO.SAVE <- "DT_Greenergy-Program_Changes_Extended.RData"
 PATH_TO.SAVE <- paste(PATH_DATA_ANALYSIS, DIR_TO.SAVE, FILE_TO.SAVE, sep = '/')
 
 
@@ -60,11 +65,11 @@ PATH_TO.SAVE <- paste(PATH_DATA_ANALYSIS, DIR_TO.SAVE, FILE_TO.SAVE, sep = '/')
 
 
 # --------------------------------------------------
-# Load SMUD Billing Data
+# Load Datasets
 # --------------------------------------------------
 # ------- Load SMUD Billing Data -------
 # # 1. Load a .RData file
-load(file = PATH_TO.LOAD_GN)
+load(file = PATH_TO.LOAD_GN_BILLING)
 
 # # 2. Check primary keys of the DT
 stopifnot(
@@ -76,148 +81,164 @@ stopifnot(
 )
 
 
-# --------------------------------------------------
-# Create a DT incl. Info. about PV Adoption and Greenergy Program Participation
-# --------------------------------------------------
-# ------- Make temporary DTs containing Greenergy-Program-related Info. -------
-# # 1. Make a DT showing each "ids"' category of program participation
-stopifnot(
-  dt_billing[
-    !is.na(category_greenergy), .N, by = .(ids, category_greenergy)
-  ][
-    , .N, by = .(ids)
-  ][N > 1] == 0
-)
-# ## Note: Only consider non-na values for "category_greenergy".
+# ------- Load SMUD Billing Data -------
+# # 1. Load a .RData file
+load(file = PATH_TO.LOAD_GN_CHANGES)
 
-tmp_dt_category <- dt_billing[
-  !is.na(category_greenergy), .N, by = .(ids, category_greenergy)
+# # 2. Check primary keys of the DT
+stopifnot(dt_rate.change[, .N, by = .(ids)][N > 1, .N] == 0)
+stopifnot(dt_suffix.change[, .N, by = .(ids)][N > 1, .N] == 0)
+
+
+# --------------------------------------------------
+# Create a DT incl. Info. about PV Adoption,
+# Greenergy Program Participation, and Residential
+# Rate
+# --------------------------------------------------
+# ------- Modify Two DTs about Greenergy Program Participation & Rate -------
+# # 1. Modify DTs before merging them
+# # 1.1. Add columns showing year-month info.
+# # 1.1.1. For "dt_suffix.change"
+cols_date <-
+  names(dt_suffix.change)[str_detect(names(dt_suffix.change), "^first.date_")]
+dt_suffix.change[
+  ,
+  (str_replace(cols_date, "^first.date", "year.month")) :=
+    lapply(.SD, as.yearmon), .SDcols = cols_date
+]
+# # 1.1.2. For "dt_rate.change"
+cols_date <-
+  names(dt_rate.change)[str_detect(names(dt_rate.change), "^first.date_")]
+dt_rate.change[
+  ,
+  (str_replace(cols_date, "^first.date", "year.month")) :=
+    lapply(.SD, as.yearmon), .SDcols = cols_date
+]
+
+
+# ------- Make temporary DTs -------
+# # 1. Make a temporary DT containing info. about PV Adoption
+tmp_dt_pv.adoption_date <- dt_billing[
+  , .N, by = .(ids, is_pv.adoption, first.date_pv.adoption_by.ids)
 ][
   , N := NULL
 ]
+tmp_dt_pv.adoption_date[
+  , year.month_pv.adoption_by.ids := as.yearmon(first.date_pv.adoption_by.ids)
+]
 
-# # 2. Make a DT showing, for each "ids", suffix code(s) & the number of billng
+
+# # 2. Make temporary DTs showing, for each "ids", the number of billng
 # #    cycles
-tmp_dt_suffix_code <- dt_billing[
-  !is.na(category_greenergy), .N, by = .(ids, suffix_greenergy)
-][
+# # 2.1. W.R.T. suffix changes
+# # 2.1.1. Add a temporary column
+dt_billing[
   ,
-  `:=` (
-    suffix_greenergy_after = shift(suffix_greenergy, n = -1),
-    n_billing.cycle_greenergy.suffix.change_after = shift(N, n = -1)
-  ),
+  tmp_first.date_suffix.change :=
+    max(first.date_suffix.change_by.ids.and.suffix, na.rm = TRUE),
   by = .(ids)
-][
-  , tmp_col_sn := 1:.N, by = .(ids)
-][
-  tmp_col_sn == 1
-][
-  , tmp_col_sn := NULL
 ]
-
-names_old <- c("N", "suffix_greenergy")
+dt_billing[
+  period_from <= tmp_first.date_suffix.change,
+  tmp_suffix_before := TRUE
+]
+dt_billing[is.na(tmp_suffix_before), tmp_suffix_before := FALSE]
+# # 2.1.2. Create a temporary DT
+tmp_dt_suffix.change <- dcast(
+  data = dt_billing[, .N, by = .(ids, tmp_suffix_before)],
+  formula = ids ~ tmp_suffix_before
+)
+names_old <- c("FALSE", "TRUE")
 names_new <- c(
-  "n_billing.cycle_greenergy.suffix.change_before", "suffix_greenergy_before"
+  "n_billing.cycle_suffix.change_after", "n_billing.cycle_suffix.change_before"
 )
-setnames(tmp_dt_suffix_code, names_old, names_new)
+setnames(tmp_dt_suffix.change, names_old, names_new)
 
-# # 3. Make a DT showing, for each "ids", the date of suffix code change
-tmp_dt_suffix_date <- dt_billing[
-  !is.na(category_greenergy), .(ids, suffix_greenergy, period_from)
-][
+# # 2.2. W.R.T. rate changes
+# # 2.2.1. Add a temporary column
+dt_billing[
   ,
-  tmp_col_suffix.change := min(period_from, na.rm = TRUE),
-  by = .(ids, suffix_greenergy)
-][
-  , date_greenergy.suffix.change := max(tmp_col_suffix.change), by = .(ids)
-][
-  , .N, by = .(ids, date_greenergy.suffix.change)
-][
-  ,
-  `:=` (
-    year.month_greenergy.suffix.change = as.yearmon(
-      date_greenergy.suffix.change
-    ),
-    N = NULL
-  )
+  tmp_first.date_rate.change :=
+    max(first.date_rate.change_by.ids.and.rate, na.rm = TRUE),
+  by = .(ids)
 ]
-
-
-# ------- Make temporary DTs containing Pv-Adoption-related Info. -------
-# # 1. Make a DT showing whether a household adopts PV system or not
-tmp_dt_pv.install <- dt_billing[, .N, by = .(ids, is_pv.install)][, N := NULL]
-
-# # 2. Make a DT showing, for each "ids", the date of PV system installation
-tmp_dt_pv.install.date <- dt_billing[
-  , .(ids, is_pv, period_from)
-][
-  ,
-  tmp_col_pv.install.date := min(period_from, na.rm = TRUE),
-  by = .(ids, is_pv)
-][
-  , date_pv.install := max(tmp_col_pv.install.date, na.rm = TRUE), by = .(ids)
-][
-  , .N, by = .(ids, date_pv.install)
-][
-  , N := NULL
+dt_billing[
+  period_from <= tmp_first.date_rate.change,
+  tmp_rate_before := TRUE
 ]
-tmp_dt_pv.install.date[, year.month_pv.install := as.yearmon(date_pv.install)]
-
-# # 3. Make a DT showing the numbers of billing cycles base on the date of PV
-# #    system installation
-tmp_dt_pv.install_billing.cycle <- dt_billing[
-  , .N, by = .(ids, is_pv)
-][
-  , n_billing.cycle_pv.install_after := shift(N, n = -1), by = .(ids)
-][
-  , tmp_col_sn := 1:.N, by = .(ids)
-][
-  tmp_col_sn == 1
-][
-  , `:=` (is_pv = NULL, tmp_col_sn = NULL)
-]
-setnames(
-  tmp_dt_pv.install_billing.cycle, "N", "n_billing.cycle_pv.install_before"
+dt_billing[is.na(tmp_rate_before), tmp_rate_before := FALSE]
+# # 2.2.2. Create a temporary DT
+tmp_dt_rate.change <- dcast(
+  data = dt_billing[, .N, by = .(ids, tmp_rate_before)],
+  formula = ids ~ tmp_rate_before
 )
+names_old <- c("FALSE", "TRUE")
+names_new <- c(
+  "n_billing.cycle_rate.change_after", "n_billing.cycle_rate.change_before"
+)
+setnames(tmp_dt_rate.change, names_old, names_new)
+
+# # 2.3. W.R.T. PV adoption
+# # 2.3.1. Add a temporary column
+dt_billing[
+  period_from <= first.date_pv.adoption_by.ids,
+  tmp_pv.adoption_before := TRUE
+]
+dt_billing[is.na(tmp_pv.adoption_before), tmp_pv.adoption_before := FALSE]
+# # 2.3.2. Create a temporary DT
+tmp_dt_pv.adoption <- dcast(
+  data = dt_billing[, .N, by = .(ids, tmp_pv.adoption_before)],
+  formula = ids ~ tmp_pv.adoption_before
+)
+names_old <- c("FALSE", "TRUE")
+names_new <- c(
+  "n_billing.cycle_pv.adoption_after", "n_billing.cycle_pv.adoption_before"
+)
+setnames(tmp_dt_pv.adoption, names_old, names_new)
 
 
-# ------- Create a DT by merging temporary DTs -------
-# # 1. Merge temporary DTs
-tmp_dt_merge.1 <- tmp_dt_pv.install.date[tmp_dt_pv.install, on = .(ids)]
-tmp_dt_merge.2 <- tmp_dt_pv.install_billing.cycle[tmp_dt_merge.1, on = .(ids)]
-tmp_dt_merge.3 <- tmp_dt_category[tmp_dt_merge.2, on = .(ids)]
-tmp_dt_merge.4 <- tmp_dt_suffix_code[tmp_dt_merge.3, on = .(ids)]
-dt_pv.and.greenergy <- tmp_dt_suffix_date[tmp_dt_merge.4, on = .(ids)]
+# ------- Create a DT by merging DTs -------
+# # 1. Merge DTs
+tmp_dt_merge.1 <- dt_suffix.change[dt_rate.change, on = .(ids)]
+tmp_dt_merge.2 <- tmp_dt_merge.1[tmp_dt_pv.adoption, on = .(ids)]
+tmp_dt_merge.3 <- tmp_dt_merge.2[tmp_dt_pv.adoption_date, on = .(ids)]
+tmp_dt_merge.4 <- tmp_dt_merge.3[tmp_dt_rate.change, on = .(ids)]
+dt_pv.and.greenergy <- tmp_dt_merge.4[tmp_dt_suffix.change, on = .(ids)]
 
 
 # # 2. Modify the DT created by merging temporary DTs
-# # 2.1. Change values of data fields
-dt_pv.and.greenergy[
-  is_pv.install == FALSE,
-  `:=` (date_pv.install = NA, year.month_pv.install = NA)
-]
-# ## Note: When "is_pv.install == FALSE", the values of "date_pv.install" and
-# ## "year.month_pv.install" are meaningless.
-
-dt_pv.and.greenergy[
-  category_greenergy %in% c("Greenergy", "Non-Greenergy", NA),
-  `:=` (
-    date_greenergy.suffix.change = NA,
-    year.month_greenergy.suffix.change = NA
-  )
-]
-# ## Note: To make date- and year-month-realted date fields mean the change in
-# ## status.
-
-# # 2.2. Change columns' order
+# # 2.1. Change columns' order
 cols_order <- c(
   "ids",
-  "is_pv.install", "date_pv.install", "year.month_pv.install",
-    "n_billing.cycle_pv.install_before", "n_billing.cycle_pv.install_after",
-  "category_greenergy", "suffix_greenergy_before", "suffix_greenergy_after",
-    "date_greenergy.suffix.change", "year.month_greenergy.suffix.change",
-    "n_billing.cycle_greenergy.suffix.change_before",
-    "n_billing.cycle_greenergy.suffix.change_after"
+  "is_pv.adoption",
+    "first.date_pv.adoption_by.ids", "year.month_pv.adoption_by.ids",
+    "n_billing.cycle_pv.adoption_before",
+    "n_billing.cycle_pv.adoption_after",
+  "category_greenergy", "n_changes_suffix",
+    "initial_suffix_greenergy",
+      "first.date_initial_suffix_greenergy",
+      "year.month_initial_suffix_greenergy",
+    "lag1_suffix_greenergy",
+      "first.date_lag1_suffix_greenergy", "year.month_lag1_suffix_greenergy",
+    "lag2_suffix_greenergy",
+      "first.date_lag2_suffix_greenergy", "year.month_lag2_suffix_greenergy",
+    "lag3_suffix_greenergy",
+      "first.date_lag3_suffix_greenergy", "year.month_lag3_suffix_greenergy",
+    "final_suffix_greenergy",
+      "first.date_final_suffix_greenergy", "year.month_final_suffix_greenergy",
+    "n_billing.cycle_suffix.change_before",
+    "n_billing.cycle_suffix.change_after",
+  "category_rate.change", "n_changes_rate",
+    "initial_rate.code",
+      "first.date_initial_rate.code", "year.month_initial_rate.code",
+    "lag1_rate.code",
+      "first.date_lag1_rate.code", "year.month_lag1_rate.code",
+    "lag2_rate.code",
+      "first.date_lag2_rate.code", "year.month_lag2_rate.code",
+    "final_rate.code",
+      "first.date_final_rate.code", "year.month_final_rate.code",
+    "n_billing.cycle_rate.change_before",
+    "n_billing.cycle_rate.change_after"
 )
 setcolorder(dt_pv.and.greenergy, cols_order)
 
