e1b70cb10f897c9d4b4e3be28a9649536032c9ad
JMJo321 <jinmahn.jo@gmail.com>
2021-05-17 17:55:01 -0700

    Add New Scripts: Template for PySpark

---
+++ 05_Code/S-Energy-Demand-Analysis_PySpark.py
@@ -0,0 +1,40 @@
+#-*- coding: utf-8 -*-
+
+"""
+* Description *
+> Script Group Indicator Number and Name
+  : (NA)
+
+> Script Number(s)
+  : (NA)
+
+> Purpose of Script
+: To set configurations of Spark Context.
+"""
+
+
+# --------------------------------------------------
+# To import module(s) and package(s) required
+# --------------------------------------------------
+import pyspark
+
+
+# --------------------------------------------------
+# To set configurations of Spark Context
+# --------------------------------------------------
+# ------- To set Spark Configuration -------
+# # 1. To set parameters
+N_CORES = 8
+DRIVER_MEMORY_GB = 15
+DRIVER_RESULTS_GB = 13
+EXECUTOR_MEMORY_GB = 5
+
+# # 2. To set Spark configuration
+pyspark_conf = (
+    pyspark.SparkConf()
+        .setMaster('local[{0}]'.format(N_CORES))
+        .set('spark.driver.memory', '{0}g'.format(DRIVER_MEMORY_GB))
+        .set('spark.driver.maxResultSize', '{0}g'.format(DRIVER_RESULTS_GB))
+        .set('spark.executor.memory', '{0}g'.format(EXECUTOR_MEMORY_GB))
+        .set('spark.sql.execution.arrow.pyspark.enabled', 'true')
+)
---
+++ 05_Code/X-00-00Z_Script-Group_Script-Template_For-PySpark.py
@@ -0,0 +1,94 @@
+#!/Users/jmjo/.pyenv/versions/energy-demand/bin/python
+#-*- coding: utf-8 -*-
+
+"""
+* Description *
+> Script Group Indicator Number and Name
+  : 
+
+> Script Number(s)
+  : 
+
+> Purpose of Script
+  : 
+
+"""
+
+
+# --------------------------------------------------
+# Load libraries required
+# --------------------------------------------------
+# ------- Load libraries -------
+# # 1. Basic libraries
+import os
+import sys
+import importlib
+import pathlib as pl
+import pandas as pd
+
+# # 2. PySpark libraries
+import pyspark
+from pyspark.sql import Column
+from pyspark.sql import DataFrame
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+# # 3. Additional libraries used in this script
+import re
+from tqdm import tqdm
+
+
+# ------- Load user-written libraries -------
+# # 1. Add a path to "sys.path", and change working directory if necessary
+if os.getcwd() == '/Users/jmjo/Dropbox/00_JMJo/Projects/Energy-Demand-Analysis':
+    sys.path.append(os.path.abspath('../Energy-Demand-Analysis/05_Code'))
+else:
+    path_toAdd = pl.Path.cwd().parents[] # TODO Must add an appropriate number
+    sys.path.append(str(path_toAdd))
+    path_project = pl.Path.cwd().parents[] # TODO Must add an appropriate number
+    os.chdir(path_project)
+
+# # 2. Load libraries
+HD = importlib.import_module('H-Energy-Demand-Analysis')
+FNC = importlib.import_module('F-Energy-Demand-Analysis_Common-Functions')
+SPARK = importlib.import_module('S-Energy-Demand-Analysis_PySpark')
+
+
+# --------------------------------------------------
+# Set path(s) and parameter(s)
+# --------------------------------------------------
+# ------- Set path(s) -------
+# # 1. Path(s) from which data file(s) will be loaded
+# (...)
+
+# # 2. Path(s) at which data file(s) will be saved
+# (...)
+
+
+# ------- Set parameter(s) -------
+# # 0. Basic Parameters
+# # 0.1. Script Number
+SCRIPT_NO = ''
+
+
+FNC.printDt('Work begins: ' + SCRIPT_NO + '\n')
+
+# --------------------------------------------------
+# Deploy PySpark
+# --------------------------------------------------
+# ------- Deploy PySpark -------
+# # 1. Spark Context
+sc = pyspark.SparkContext(conf=SPARK.pyspark_conf)
+
+# # 2. Spark SQL Context
+sqlc = pyspark.SQLContext(sc)
+
+
+# --------------------------------------------------
+# ...
+# --------------------------------------------------
+# ------- ... -------
+# (...)
+
+FNC.printDt('Work ends: ' + SCRIPT_NO + '\n')
