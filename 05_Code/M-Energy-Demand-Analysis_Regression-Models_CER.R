c7ae4e79fe97a71d48d596ba2e6ff1f4ff2df639
JMJo321 <jinmahn.jo@gmail.com>
2021-07-27 13:59:39 -0700

    Add econometric models that utilize hourly data (#4)

--- 05_Code/M-Energy-Demand-Analysis_Regression-Models_CER.R
+++ 05_Code/M-Energy-Demand-Analysis_Regression-Models_CER.R
@@ -22,10 +22,9 @@
 # --------------------------------------------------
 # Define Regression Models
 # --------------------------------------------------
-# ------- Models for Estimating Temperature Response -------
+# ------- Models for Estimating Temp. Response: with Daily Consumption -------
 # # 1. Models without FEs
-# # 1.1. Models for Day-level Data
-# # 1.1.1. Models for a Sample excluding Observations of Control Group
+# # 1.1. Models for a Sample excluding Observations of Control Group
 model_ols_daily_excl.control_linear <- formula(
   kwh ~
     hdd_all + treatment.and.post + hdd_all:treatment.and.post |
@@ -41,7 +40,7 @@ model_ols_daily_excl.control_quadratic <- formula(
       0 |
       id.and.day.of.week_in.factor + month_in.factor
 )
-# # 1.1.2. Models for a Sample including Observations of Control Group
+# # 1.2. Models for a Sample including Observations of Control Group
 model_ols_daily_incl.control_linear <- formula(
   kwh ~
     hdd_all + is_treated_r + hdd_all:is_treated_r +
@@ -66,8 +65,7 @@ model_ols_daily_incl.control_quadratic <- formula(
 
 
 # # 2. Models with FEs
-# # 2.1. Models for Day-level Data
-# # 2.1.1. Models for a Sample excluding Observations of Control Group
+# # 2.1. Models for a Sample excluding Observations of Control Group
 model_fes_daily_excl.control_linear <- formula(
   kwh ~
     hdd_all + treatment.and.post + hdd_all:treatment.and.post |
@@ -83,7 +81,7 @@ model_fes_daily_excl.control_quadratic <- formula(
     0 |
     id.and.day.of.week_in.factor + month_in.factor
 )
-# # 2.1.2. Models for a Sample including Observations of Control Group
+# # 2.2. Models for a Sample including Observations of Control Group
 model_fes_daily_incl.control_linear <- formula(
   kwh ~
     hdd_all + is_treated_r + hdd_all:is_treated_r +
@@ -122,6 +120,65 @@ model_fes_daily_incl.control_quadratic_variation1 <- formula(
         I(hdd_all^2):is_treatment.period +
       treatment.and.post + hdd_all:treatment.and.post +
         I(hdd_all^2):treatment.and.post |
+    id.and.day.of.week_in.factor + month_in.factor |
+    0 |
+    id.and.day.of.week_in.factor + month_in.factor
+)
+
+
+# ------- Models for Estimating Temp. Response: with Hourly Consumption -------
+# # 1. Models without FEs
+model_ols_hourly <- formula(
+  kwh ~
+    hd_by.season.and.rate.period:rate.period +
+      I(hd_by.season.and.rate.period^2):rate.period +
+    is_treated_r:rate.period +
+      hd_by.season.and.rate.period:is_treated_r:rate.period +
+      I(hd_by.season.and.rate.period^2):is_treated_r:rate.period +
+    is_treatment.period:rate.period +
+      hd_by.season.and.rate.period:is_treatment.period:rate.period +
+      I(hd_by.season.and.rate.period^2):is_treatment.period:rate.period +
+    treatment.and.post:rate.period +
+      hd_by.season.and.rate.period:treatment.and.post:rate.period +
+      I(hd_by.season.and.rate.period^2):treatment.and.post:rate.period |
+    0 |
+    0 |
+    id.and.day.of.week_in.factor + month_in.factor
+)
+
+
+# # 2. Models with FEs
+model_fes_hourly_base <- formula(
+  kwh ~
+    hd_by.season.and.rate.period:rate.period +
+      I(hd_by.season.and.rate.period^2):rate.period +
+    is_treated_r:rate.period +
+      hd_by.season.and.rate.period:is_treated_r:rate.period +
+      I(hd_by.season.and.rate.period^2):is_treated_r:rate.period +
+    is_treatment.period:rate.period +
+      hd_by.season.and.rate.period:is_treatment.period:rate.period +
+      I(hd_by.season.and.rate.period^2):is_treatment.period:rate.period +
+    treatment.and.post:rate.period +
+      hd_by.season.and.rate.period:treatment.and.post:rate.period +
+      I(hd_by.season.and.rate.period^2):treatment.and.post:rate.period |
+    id.and.day.of.week_in.factor |
+    0 |
+    id.and.day.of.week_in.factor + month_in.factor
+)
+
+model_fes_hourly_var1 <- formula(
+  kwh ~
+    hd_by.season.and.rate.period:rate.period +
+      I(hd_by.season.and.rate.period^2):rate.period +
+    is_treated_r:rate.period +
+      hd_by.season.and.rate.period:is_treated_r:rate.period +
+      I(hd_by.season.and.rate.period^2):is_treated_r:rate.period +
+    is_treatment.period:rate.period +
+      hd_by.season.and.rate.period:is_treatment.period:rate.period +
+      I(hd_by.season.and.rate.period^2):is_treatment.period:rate.period +
+    treatment.and.post:rate.period +
+      hd_by.season.and.rate.period:treatment.and.post:rate.period +
+      I(hd_by.season.and.rate.period^2):treatment.and.post:rate.period |
     id.and.day.of.week_in.factor + month_in.factor |
     0 |
     id.and.day.of.week_in.factor + month_in.factor
